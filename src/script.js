c143620ba50f2f5b75a03a4cdc40bde2264f6e59
Denver-Meadows <meadows8200@gmail.com>
2021-08-01 16:54:47 -0400

    Adding todos working

--- src/script.js
+++ src/script.js
@@ -1,18 +1,20 @@
 const addToDoBtn = document.querySelector('.todo__form__button');
-const completeBtn = document.querySelectorAll('.todo__complete');
+const completeBtn = document.querySelector('.todo__complete');
 const editBtn = document.querySelector('.todo__edit');
 const deleteBtn = document.querySelector('.todo__delete');
 const mainToDoList = document.querySelector('.todo__main__list');
 const todoList = document.querySelector('.todo__list')
+const todoText = document.querySelector('.todo__text')
 
 const state = {
-  currentCompleteBtnIndex: '',
+  todos: [],
+  completedToDo: [],
 }
 
 const completeToDo = function(btn) {
-  // add line-through class to current selected complete-btn
-  // completeBtn[state.currentCompleteBtnIndex].parentNode.classList.toggle('line-through')
-  btn.parentNode.classList.toggle('line-through')
+  btn.parentNode.classList.toggle('line-through');
+  state.completedToDo.push(btn.closest('.todo__text'));
+
 };
 
 const editToDo = function() {
@@ -23,13 +25,34 @@ const deleteToDo = function() {
 
 };
 
+addToDoBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  const value = document.querySelector('.todo__form__input').value
+  state.todos.push(value)
+  renderNewTodo(value)
+  console.log(state.todos)
+  document.querySelector('.todo__form__input').value = ''
+});
+
 todoList.addEventListener('click', (e) => {
   const btn = e.target.closest('button')
   if (!btn) return;
   if (btn.classList.contains('todo__delete')) console.log('delete that');
   if (btn.classList.contains('todo__edit')) console.log('edit that');
   if (btn.classList.contains('todo__complete')) completeToDo(btn);
-})
+});
+
+const renderNewTodo = function(todo) {
+  const html = `
+    <li>
+      <span class="todo__text">${todo}</span>
+      <button class="todo__button todo__delete"><i class="fas fa-trash"></i></button>
+      <button class="todo__button todo__edit"><i class="fas fa-edit"></i></button>
+      <button class="todo__button todo__complete"><i class="fas fa-check"></i></button>
+    </li>
+  `
+  todoList.insertAdjacentHTML('beforeend', html)
+}
 
 // Add event listener to each btn
 // It's better to use event bubbling hear.
